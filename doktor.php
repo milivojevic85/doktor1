09021bcc9d8b23fe4377718f18ceb1ed281c64f0
milivojevic85 <misagrf@yahoo.com>
2020-01-28 13:00:32 +0100

    logovanje i probanja

--- doktor.php
+++ doktor.php
@@ -30,6 +30,10 @@ class Doktor extends Logovanje
 		$this->ispis = $this->trenutno();
 	}
 
+	public function getIme() {
+		return $this->ime;
+	}
+
 	public function zakaziPregled(Pacijent $pacijent, Pregled $pregled) {}
 }
 
@@ -38,6 +42,7 @@ class Pacijent extends Logovanje
 	private $ime, $prezime, $jmbg, $bzk;
 	private $ispis;
 	private $doktor = null;
+	private $pregled = null;
 	public function __construct($ime, $prezime, $jmbg, $bzk) {
 		$this->ime = $ime;
 		$this->prezime = $prezime;
@@ -47,30 +52,48 @@ class Pacijent extends Logovanje
 		$this->ispis = $this->trenutno();
 	}
 	public function izaberiLekara(Doktor $doktor) {
-		// logovanje
+		$this->details = $this->ime. " bira doktora ".$doktor->getIme() ;
+		$this->ispis = $this->trenutno();
 	}
-	public function obaviPregled(Pregled $pregled) {
-		// logovanje
+	public function obavljaPregled(Pregled $pregled) {
+		$this->details = $this->ime. " obavlja laboratorijski pregled ".$pregled->getIme() ;
+		$this->ispis = $this->trenutno();
 	}
 }
 
 abstract class Pregled
 {
 	protected $datum, $vreme;
+	abstract public function getIme();
 }
 
 class KrvniPritisak extends Pregled {
 	private $gornjaVrednost, $donjaVrednost, $puls;
+	public function getIme() {
+		return "Krvni pritisak";
+	}
 }
 class SecerUKrvi extends Pregled {
 	private $vrednost, $vremePoslObroka;
+	
+	public function getIme() {
+		return "Secer u krvi";
+	}
 }
 class HolesterolUKrvi extends Pregled {
 	private $vrednost, $vremePoslObroka;
+	public function getIme() {
+		return "Holesterol u krvi";
+	}
 }
 
-$doktor_1 = new Doktor("Milan", "Markovic", "sve i svasta");
+$doktor_1 = new Doktor("Marko", "Markovic", "sve i svasta");
 $pacijent_1 = new Pacijent("Dragan", "Jovanovic", "1232343458787", "344898");
+$pacijent_1->izaberiLekara($doktor_1);
+
+$pregled_secerUkrvi_1 = new SecerUKrvi();
+$pacijent_1->obavljaPregled($pregled_secerUkrvi_1);
+
 
 ?>
 
