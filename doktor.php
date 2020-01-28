8bacd6649f0aa4f6442db1088fa34bc65adad7e4
milivojevic85 <misagrf@yahoo.com>
2020-01-28 09:59:20 +0100

    skica klasa, svojstava

--- doktor.php
+++ doktor.php
@@ -7,8 +7,33 @@
 <body>	
 
 <?php
+class Doktor
+{
+	private $ime, $prezime, $specijalnost;
+	
+	public function zakaziPregled(Pacijent $pacijent, Pregled $pregled)
+}
 
+class Pacijent
+{
+	private $ime, $prezime, $jmbg, $bzk;
+	private $doktor = null;
+}
 
+abstract class Pregled
+{
+	protected $datum, $vreme;
+}
+
+class KrvniPritisak extends Pregled {
+	private $gornjaVrednost, $donjaVrednost, $puls;
+}
+class SecerUKrvi extends Pregled {
+	private $vrednost, $vremePoslObroka;
+}
+class HolesterolUKrvi extends Pregled {
+	private $vrednost, $vremePoslObroka;
+}
 ?>
 
 </body>
